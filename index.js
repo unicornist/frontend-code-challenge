0727ff4a163b2a18c538b5bacee1769253f9c620
Babak Khorrami <k.babak@gmail.com>
2018-12-03 00:40:52 +0330

    move all routes under /api
    to prevent route conflicting with UI routes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.use('/items', items);
+app.use('/api/items', items);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
